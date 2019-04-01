128aa79d12f9da222de6928d409cbc6a545cba16
benny <oseguerabenny27@gmail.com>
2019-03-31 20:42:05 -0700

    Fixed: Typo in one of the imports

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./components/App/App";
+import App from "./App";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
