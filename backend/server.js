8b5b1f846ad0e5ca5e3808b5a6f7eb43d6845fad
David Benavidez <benavidez_david13@yahoo.com>
2019-04-01 13:13:40 -0700

    adding dependencies

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+// DEPENDENCIES
+const express = require('express');
+const server = express();
+const cors = require('cors');
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+  res.send('Hello from Express');
+});
+
+
+
+module.exports = server;
